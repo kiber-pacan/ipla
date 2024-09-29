33f7e1b59d57f3d5552f6d5a03a1ebfc11a582bd
kiber-pacan <markpustog@gmail.com>
2024-09-29 12:52:51 +0100

    Refactoring

--- common/src/main/java/com/akicater/network/ItemPlacePayload.java
+++ common/src/main/java/com/akicater/network/ItemPlacePayload.java
@@ -2,28 +2,21 @@ package com.akicater.network;
 
 import com.akicater.Ipla;
 import com.akicater.blocks.LayingItemEntity;
-import com.mojang.serialization.Codec;
-import dev.architectury.hooks.level.biome.EffectsProperties;
-import dev.architectury.impl.NetworkAggregator;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
-import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.common.custom.BrainDebugPayload;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.NotNull;
 
 import static com.akicater.Ipla.MOD_ID;
 
@@ -32,41 +25,66 @@ public record ItemPlacePayload(BlockPos pos, BlockHitResult hitResult) implement
     public static final StreamCodec<FriendlyByteBuf, ItemPlacePayload> CODEC = StreamCodec.of((buf, value) -> buf.writeBlockPos(value.pos).writeBlockHitResult(value.hitResult), buf -> new ItemPlacePayload(buf.readBlockPos(), buf.readBlockHitResult()));
 
     @Override
-    public Type<? extends CustomPacketPayload> type() {
+    public @NotNull Type<? extends CustomPacketPayload> type() {
         return TYPE;
     }
 
     public static void receive(Player player, BlockPos pos, BlockHitResult hitResult) {
-        Level level = player.level();
         ItemStack stack = player.getMainHandItem();
-        if (level.getBlockState(pos).getBlock() == Blocks.AIR || level.getBlockState(pos).getBlock() == Blocks.WATER) {
-            player.setItemInHand(InteractionHand.MAIN_HAND, ItemStack.EMPTY);
-            Direction dir = hitResult.getDirection().getOpposite();
+        if (stack == ItemStack.EMPTY) return;
+
+        Level level = player.level();
+        BlockPos tempPos = pos;
+        if (level.getBlockState(pos).getBlock() != Ipla.lItemBlock.get()) {
+            tempPos = pos.relative(hitResult.getDirection(), 1);
+        }
+
+        Block replBlock = level.getBlockState(tempPos).getBlock();
+
+        if (replBlock == Blocks.AIR || replBlock == Blocks.WATER) {
             BlockState state = Ipla.lItemBlock.get().defaultBlockState();
-            if (stack == ItemStack.EMPTY) return;
-            if (level.getBlockState(pos).getBlock() == Blocks.WATER) {
+
+            if (replBlock == Blocks.WATER) {
                 state = state.setValue(BlockStateProperties.WATERLOGGED, true);
             }
-            level.setBlock(pos, state,0);
-            state.initCache();
-            LayingItemEntity blockEntity = (LayingItemEntity)level.getChunk(pos).getBlockEntity(pos);
-            if (blockEntity != null) {
-                int i = dir.get3DDataValue();
-                blockEntity.items.set(i, stack);
-                level.gameEvent(player, GameEvent.BLOCK_CHANGE, pos);
-                //blockEntity.markDirty();
-            }
-        } else if (level.getBlockState(pos).getBlock() == Ipla.lItemBlock.get()) {
-            Direction dir = hitResult.getDirection().getOpposite();
-            LayingItemEntity blockEntity = (LayingItemEntity)level.getChunk(pos).getBlockEntity(pos);
-            if (blockEntity != null) {
-                int i = dir.get3DDataValue();
-                if(blockEntity.items.get(i).isEmpty()) {
-                    player.setItemInHand(InteractionHand.MAIN_HAND, ItemStack.EMPTY);
-                    blockEntity.items.set(i, stack);
-                    level.gameEvent(player, GameEvent.BLOCK_CHANGE, pos);
-                    //blockEntity.markDirty();
+
+            level.setBlockAndUpdate(tempPos, state);
+            LayingItemEntity entity = (LayingItemEntity)level.getBlockEntity(tempPos);
+
+            if (entity != null) {
+                int i = hitResult.getDirection().get3DDataValue();
+
+                if (player.isCrouching()) {
+                    entity.inv.set(Ipla.getIndexFromHit(hitResult, true), stack);
+                    entity.quad.set(i, true);
+                } else {
+                    entity.inv.set(i * 4, stack);
                 }
+                player.setItemInHand(InteractionHand.MAIN_HAND, ItemStack.EMPTY);
+            }
+
+        } else if (level.getBlockState(tempPos).getBlock() == Ipla.lItemBlock.get()) {
+            LayingItemEntity entity = (LayingItemEntity)level.getBlockEntity(tempPos);
+
+            if (entity != null) {
+                int i = hitResult.getDirection().get3DDataValue();
+
+                if (player.isCrouching()) {
+                    int x = Ipla.getIndexFromHit(hitResult, (level.getBlockState(pos).getBlock()!= Ipla.lItemBlock.get()));
+                    if(entity.inv.get(x).isEmpty()) {
+                        entity.inv.set(x, stack);
+                        entity.quad.set(i, true);
+                        player.setItemInHand(InteractionHand.MAIN_HAND, ItemStack.EMPTY);
+                    }
+                } else {
+                    if(entity.inv.get(i * 4).isEmpty()) {
+                        entity.inv.set(i * 4, stack);
+                        player.setItemInHand(InteractionHand.MAIN_HAND, ItemStack.EMPTY);
+                    }
+                }
+
+                entity.markDirty();
+
             }
         }
     }
