55fbcc8bd211a0aff2bd6f3c03daff32dbf3f3c3
kiber-pacan <markpustog@gmail.com>
2024-09-29 12:53:15 +0100

    Registering client stuff

--- fabric/src/main/java/com/akicater/fabric/client/IplaFabricClient.java
+++ fabric/src/main/java/com/akicater/fabric/client/IplaFabricClient.java
@@ -1,10 +1,15 @@
 package com.akicater.fabric.client;
 
+import com.akicater.Ipla;
+import dev.architectury.registry.client.rendering.BlockEntityRendererRegistry;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 
 public final class IplaFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+        Ipla.initiliazeClient();
+        BlockEntityRenderers.register(Ipla.lItemBlockEntity.get(), LayingItemBER_fabric::new);
     }
 }
