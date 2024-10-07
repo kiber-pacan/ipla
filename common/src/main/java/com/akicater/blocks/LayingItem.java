a23939eb88d5f2e86314f4d067ceb1887dbbd475
kiber-pacan <markpustog@gmail.com>
2024-10-07 18:56:01 +0100

    Better using algorithm

--- common/src/main/java/com/akicater/blocks/LayingItem.java
+++ common/src/main/java/com/akicater/blocks/LayingItem.java
@@ -3,27 +3,28 @@ package com.akicater.blocks;
 import com.akicater.Ipla;
 import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.MapCodec;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.Containers;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.FurnaceBlock;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.ChiseledBookShelfBlockEntity;
-import net.minecraft.world.level.block.entity.FurnaceBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+
+import java.util.function.ToIntFunction;
+
+import static com.akicater.Ipla.HIDE_ITEM_KEY;
 
 public class LayingItem extends BaseEntityBlock {
     public static final MapCodec<LayingItem> CODEC = simpleCodec(LayingItem::new);
@@ -43,6 +44,10 @@ public class LayingItem extends BaseEntityBlock {
     }
 
 
+    @Override
+    protected void spawnDestroyParticles(Level level, Player player, BlockPos pos, BlockState state) {}
+
+
     // Get voxel shape of placed items
     @Override
     protected @NotNull VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
@@ -54,6 +59,7 @@ public class LayingItem extends BaseEntityBlock {
 
         return super.getShape(state, level, pos, context);
     }
+
 
     // Drop items on break
     @Override
@@ -80,9 +86,10 @@ public class LayingItem extends BaseEntityBlock {
         }
     }
 
-    public static void dropItem(Level level, BlockPos pos, Pair<Integer, Integer> si, LayingItemEntity entity) {
-        int s = si.getFirst();
-        int i = si.getSecond();
+    public static void dropItem(Level level, BlockPos pos, Pair<Integer, Integer> pair, LayingItemEntity entity) {
+        int s = pair.getFirst();
+        int i = pair.getSecond();
+
         if (entity.quad.get(s)) {
             Containers.dropItemStack(level, pos.getX(), pos.getY(), pos.getZ(), entity.inv.get(s * 4 + i));
         } else {
@@ -90,15 +97,32 @@ public class LayingItem extends BaseEntityBlock {
         }
     }
 
+
     @Override
     protected @NotNull InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hit) {
         LayingItemEntity entity = (LayingItemEntity) level.getBlockEntity(pos);
         if (entity != null) {
-            dropItem(level, pos, Ipla.getIndexFromHit(hit, false), entity);
+            Pair<Integer, Integer> pair = Ipla.getIndexFromHit(hit, false);
+            int x = pair.getFirst();
+            int i = pair.getSecond();
+
+            if (player.getMainHandItem().isEmpty()) {
+                if (!entity.quad.get(x)) i = 0;
+
+                player.setItemInHand(InteractionHand.MAIN_HAND, entity.inv.get(x * 4 + i));
+                entity.inv.set(x * 4 + i, ItemStack.EMPTY);
+                level.playSeededSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.BUNDLE_REMOVE_ONE, SoundSource.BLOCKS, 1, 1.4f, 1);
+            } else {
+                dropItem(level, pos, pair, entity);
+                level.playSeededSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.BUNDLE_REMOVE_ONE, SoundSource.BLOCKS, 1, 1.4f, 1);
+            }
 
             if (entity.isEmpty()) {
                 level.removeBlock(pos, false);
                 entity.setRemoved();
+            } else if (entity.isSlotEmpty(x)) {
+                entity.quad.set(x, false);
+                entity.markDirty();
             }
 
             return InteractionResult.SUCCESS;
@@ -107,3 +131,5 @@ public class LayingItem extends BaseEntityBlock {
         }
     }
 }
+
+
