3516354ea79dc1c74574fe9e2dcaeb8ff4a55040
kiber-pacan <markpustog@gmail.com>
2024-10-07 18:54:29 +0100

    Better rendering

--- common/src/main/java/com/akicater/client/LayingItemBER_common.java
+++ common/src/main/java/com/akicater/client/LayingItemBER_common.java
@@ -9,6 +9,7 @@ import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.NonNullList;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
@@ -25,6 +26,7 @@ import org.joml.Vector3f;
 
 public abstract class LayingItemBER_common implements BlockEntityRenderer<LayingItemEntity> {
     public LayingItemBER_common(BlockEntityRendererProvider.Context context) {
+
     }
 
     public static List<Quaternionf> rot = new ArrayList<>(
@@ -39,24 +41,6 @@ public abstract class LayingItemBER_common implements BlockEntityRenderer<Laying
     );
 
     public static Vec3 pos1 = new Vec3(0.5F, 0.5F, 0);
-
-    public static Quaternionf rotateZ(float angle, Quaternionf dest) {
-        float sin = Math.sin(angle * 0.5f);
-        float cos = Math.cosFromSin(sin, angle * 0.5f);
-        return new Quaternionf(dest.x * cos + dest.y * sin,
-                dest.y * cos - dest.x * sin,
-                dest.w * sin + dest.z * cos,
-                dest.w * cos - dest.z * sin);
-    }
-
-    public static Quaternionf rotateX(float angle, Quaternionf dest) {
-        float sin = Math.sin(angle * 0.5f);
-        float cos = Math.cosFromSin(sin, angle * 0.5f);
-        return new Quaternionf(dest.w * sin + dest.x * cos,
-                dest.y * cos + dest.z * sin,
-                dest.z * cos - dest.y * sin,
-                dest.w * cos - dest.x * sin);
-    }
 
     public void render(LayingItemEntity entity, float partialTick, PoseStack poseStack, MultiBufferSource buffer, int packedLight, int packedOverlay, float absoluteSize, float blockSize, float itemSize, boolean oldRendering) {
         ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
@@ -73,7 +57,7 @@ public abstract class LayingItemBER_common implements BlockEntityRenderer<Laying
                         poseStack.pushPose();
 
                         boolean fullBlock = stack.getItem() instanceof BlockItem && ((BlockItem) stack.getItem()).getBlock().defaultBlockState().isCollisionShapeFullBlock(entity.getLevel(), entity.getBlockPos());
-                        manipStack(poseStack, entity, fullBlock, oldRendering, iSize, s, i);
+                        manipStack(poseStack, entity, fullBlock, oldRendering, bSize, iSize, s, i);
 
                         if ((fullBlock)) {
                             poseStack.scale(bSize, bSize, bSize);
@@ -93,7 +77,7 @@ public abstract class LayingItemBER_common implements BlockEntityRenderer<Laying
                     poseStack.pushPose();
 
                     boolean fullBlock = stack.getItem() instanceof BlockItem && ((BlockItem) stack.getItem()).getBlock().defaultBlockState().isCollisionShapeFullBlock(entity.getLevel(), entity.getBlockPos());
-                    manipStack(poseStack, entity, fullBlock, oldRendering, itemSize, s, 4);
+                    manipStack(poseStack, entity, fullBlock, oldRendering, bSize, itemSize, s, 4);
 
                     if ((fullBlock)) {
                         poseStack.scale(blockSize, blockSize, blockSize);
@@ -110,28 +94,41 @@ public abstract class LayingItemBER_common implements BlockEntityRenderer<Laying
     }
 
 
-    public static void manipStack(PoseStack poseStack, LayingItemEntity entity, boolean fullBlock, boolean oldRendering, float bSize, int s, int i) {
-        // If full block and old rendering disabled rotate 90 degrees
-        poseStack.translate(0.5, 0, 0.5);
+    public void manipStack(PoseStack poseStack, LayingItemEntity entity, boolean fullBlock, boolean oldRendering, float bSize, float iSize, int s, int i) {
+        poseStack.translate(0.5, 0.5, 0.5);
 
-        poseStack.mulPose(rotateZ(Math.toRadians(entity.rot.get(s * 4)), rot.get(s)));
+        poseStack.mulPose(rot.get(s));
+
+        boolean quad = i < 4;
+        int x = s * 4 + ((quad) ? i : 0);
+
+        float rotation = Math.lerp(entity.lastRot.get(x), entity.rot.get(x), 0.1f);
 
         if (fullBlock && !oldRendering) {
-            poseStack.translate(-0.5, 0, -0.5);
+            poseStack.translate(-0.5, -0.5, -0.5);
+
+            if (quad) {
+                poseStack.translate(0.25f + (((i + 1) % 2 == 0) ? 0.5f : 0), 0.25f + ((i > 1) ? 0.5f : 0), 0.25f * bSize);
+            } else {
+                poseStack.translate(pos1.x, pos1.y, 0.5f * bSize);
+            }
+
+            poseStack.mulPose(Axis.ZP.rotationDegrees(rotation));
             poseStack.mulPose(Axis.XP.rotationDegrees(90));
-            if (i < 4) {
-                poseStack.translate(0.25f + (((i + 1) % 2 == 0) ? 0.5f : 0), 0.25 * bSize, -(0.25f + ((i > 1) ? 0.5f : 0)));
-            } else {
-                poseStack.translate(pos1.x, 0.25 * bSize, -pos1.y);
-            }
         } else {
-            poseStack.translate(-0.5, 0, -0.5);
-            if (i < 4) {
-                poseStack.translate(0.25f + (((i + 1) % 2 == 0) ? 0.5f : 0), 0.25f + ((i > 1) ? 0.5f : 0), 0);
+            poseStack.translate(-0.5, -0.5, -0.5);
+
+            if (quad) {
+                poseStack.translate(0.25f + (((i + 1) % 2 == 0) ? 0.5f : 0), 0.25f + ((i > 1) ? 0.5f : 0), 0.025f * iSize);
             } else {
-                poseStack.translate(pos1.x, pos1.y, 0);
+                poseStack.translate(pos1.x, pos1.y, 0.025f * iSize);
             }
+
+            poseStack.mulPose(Axis.ZP.rotationDegrees(rotation));
+            poseStack.mulPose(Axis.YP.rotationDegrees(180));
         }
+
+        entity.lastRot.set(x, rotation);
     }
 
     public abstract void render(LayingItemEntity entity, float partialTick, PoseStack poseStack, MultiBufferSource buffer, int packedLight, int packedOverlay);
