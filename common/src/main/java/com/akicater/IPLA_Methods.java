60533bd4f219cb2db847975b64f37a758ebc213a
akicatt <markpustog@gmail.com>
2026-07-30 23:56:15 +0500

    revamped placement and added item like checking

--- common/src/main/java/com/akicater/IPLA_Methods.java
+++ common/src/main/java/com/akicater/IPLA_Methods.java
@@ -1,24 +1,36 @@
 package com.akicater;
 
 import com.akicater.blocks.LayingItemEntity;
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.Vec3i;
 import net.minecraft.core.particles.ItemParticleOption;
 import net.minecraft.core.particles.ParticleTypes;
 #if MC_VER >= V1_19_2
 import net.minecraft.util.RandomSource;
 #else
-import java.util.Random;
+import java.lang.reflect.Field;
+import java.util.*;
 #endif
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.reflect.Field;
+import java.util.*;
 
 
 public class IPLA_Methods {
@@ -57,17 +69,6 @@ public class IPLA_Methods {
         ((EatingPlayer) player).ipla$setLayingItemEntity(null);
     }
 
-    static List<AABB> boxes = new ArrayList<>(
-            List.of(
-                    new AABB(0.0, 1.0 - 1.0 / 16 * 4, 0.0, 1.0, 1.0, 1.0), // TOP
-                    new AABB(0.0, 0.0, 0.0, 1.0, 1.0 / 16 * 4, 1.0), // DOWN
-                    new AABB(0.0, 0.0, 1.0 - 1.0 / 16 * 4, 1.0, 1.0, 1.0), // SOUTH
-                    new AABB(0.0, 0.0, 0, 1.0, 1.0, 1.0 / 16 * 4), // NORTH
-                    new AABB(1.0 / 16 * 4, 0.0, 0.0, 1.0 / 16 * 4, 1.0f, 1.0f), // WEST
-                    new AABB(1.0 - 1.0 / 16 * 4, 0.0, 0.0, 1.0 - 1.0 / 16 * 4, 1.0f, 1.0f) // EAST
-            )
-    );
-
     public static BlockHitResult getBlockHitResult(HitResult hit) {
         if (hit.getType() == HitResult.Type.BLOCK) {
             return (BlockHitResult) hit;
@@ -81,18 +82,6 @@ public class IPLA_Methods {
         return x >= box.minX - EPS && x <= box.maxX + EPS
                 && y >= box.minY - EPS && y <= box.maxY + EPS
                 && z >= box.minZ - EPS && z <= box.maxZ + EPS;
-    }
-
-
-    public static int getSlotFromShape(double x, double y, double z) {
-        List<Integer> slots = new ArrayList<>();
-        for (int i = 0; i < boxes.size(); i++) {
-            if (contains(x, y, z, boxes.get(i))) {
-                return i;
-            }
-        }
-
-        return -1;
     }
 
     public static int getSubSlotFromPos(int slot, double x, double y, double z) {
@@ -111,26 +100,40 @@ public class IPLA_Methods {
         return 0;
     }
 
-    public static int getSlotFromHit(BlockHitResult hit, boolean empty, boolean quad) {
-        BlockPos blockPos = hit.getBlockPos();
-        Vec3 pos = hit.getLocation();
+    public static Pair<Integer, Boolean> getSlotFromHit(BlockHitResult hitResult, boolean isLayingItem, LayingItemEntity entity, Player player) {
+        Vec3 location = hitResult.getLocation();
+        Direction direction = hitResult.getDirection();
+        Vec3 relativeLocation =
+                location.add(
+                        direction.getStepX() * 0.249,
+                        direction.getStepY() * 0.249,
+                        direction.getStepZ() * 0.249
+                );
 
-        double x = Math.abs(pos.x - blockPos.getX());
-        double y = Math.abs(pos.y - blockPos.getY());
-        double z = Math.abs(pos.z - blockPos.getZ());
+        double x = relativeLocation.x - Math.floor(relativeLocation.x);
+        double y = relativeLocation.y - Math.floor(relativeLocation.y);
+        double z = relativeLocation.z - Math.floor(relativeLocation.z);
+
 
         int slot;
+        boolean quad;
+        int directionIndex;
 
-        if (empty) {
-            slot = hit.getDirection().get3DDataValue();
+
+        if (!isLayingItem) {
+            directionIndex = direction.get3DDataValue();
         } else {
-            slot = getSlotFromShape(x, y, z);
+            directionIndex = direction.getOpposite().get3DDataValue();
         }
 
-        return slot * 4 + ((quad) ? getSubSlotFromPos(slot, x, y, z) : 0);
+        slot = directionIndex;
+
+        quad = entity.quad.get(directionIndex) || player.isDiscrete();
+
+        return Pair.of(slot * 4 + ((quad) ? getSubSlotFromPos(slot, x, y, z) : 0), quad);
     }
 
-    public static List<Integer> getPreciseIndexFromHit(LayingItemEntity entity, BlockHitResult hit, Boolean empty) {
+    public static List<Integer> getPreciseIndexFromHit(LayingItemEntity entity, BlockHitResult hit) {
         List<Integer> list = new ArrayList<>(0);
 
         for (int i = 0; i < entity.inv.size(); i++) {
@@ -159,5 +162,80 @@ public class IPLA_Methods {
 
     public static int getIndexFromXY(double a, double b) {
         return ((a > 0.5) ? 1 : 0) + ((b > 0.5) ? 2 : 0);
+    }
+
+    static Set<Block> BER_cache = new HashSet<>();
+
+    public static boolean hasBER(BlockState state) {
+        BlockEntityRenderDispatcher dispatcher = Minecraft.getInstance().getBlockEntityRenderDispatcher();
+
+        try {
+            Field field = BlockEntityRenderDispatcher.class.getDeclaredField("renderers");
+            field.setAccessible(true);
+
+            @SuppressWarnings("unchecked")
+            #if MC_VER >= V1_21_9
+            Map<BlockEntityType<?>, BlockEntityRenderer <?, ?>> renderers = (Map<BlockEntityType<?>, BlockEntityRenderer<?, ?>>) field.get(dispatcher);
+            #else
+            Map<BlockEntityType<?>, BlockEntityRenderer <?>> renderers = (Map<BlockEntityType<?>, BlockEntityRenderer<?>>) field.get(dispatcher);
+            #endif
+
+            for (BlockEntityType<?> type : renderers.keySet()) {
+                if (type.isValid(state)) return true;
+            }
+
+            return false;
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            return true;
+        }
+    }
+
+    public static boolean isItemLike(Block block) {
+        return block instanceof CrossCollisionBlock
+                || block instanceof FenceBlock
+                || block instanceof FenceGateBlock
+                || block instanceof WallBlock
+                || block instanceof DoorBlock
+                || block instanceof BedBlock
+                || block instanceof RedStoneWireBlock
+                || block instanceof LadderBlock
+                || block instanceof TripWireHookBlock
+                || block instanceof VineBlock
+                || block instanceof GlowLichenBlock
+                || block instanceof BaseRailBlock
+                || block instanceof ButtonBlock
+                || block instanceof LeverBlock
+                || block instanceof TripWireBlock ||
+                #if MC_VER >= V1_21_9
+                block instanceof ShelfBlock ||
+                #endif
+                block instanceof ChestBlock
+                ;
+    }
+
+    public static boolean isCuboid(Item item) {
+        boolean isBlock = item instanceof BlockItem;
+        boolean cuboid = false;
+        if (isBlock) {
+            Block block = ((BlockItem) item).getBlock();
+            cuboid = !isItemLike(block);
+        }
+
+        return cuboid;
+    }
+
+    public static boolean renderAsBlock(Item item) {
+        boolean isBlock = item instanceof BlockItem;
+        boolean hasBER = false;
+        boolean isBadBlockModel = false;
+
+        if (isBlock) {
+            Block block = ((BlockItem) item).getBlock();
+            hasBER = BER_cache.contains(block) || hasBER(block.defaultBlockState());
+            if (hasBER) BER_cache.add(block);
+
+            isBadBlockModel = isItemLike(block);
+        }
+        return isBlock && !hasBER && !isBadBlockModel;
     }
 }
